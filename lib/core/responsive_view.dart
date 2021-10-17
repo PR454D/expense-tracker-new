5cbdcb391de2b6be4d186dbdeae11de178bc50b1
Husain Lokhandwala <60429312+Lokhandwalah@users.noreply.github.com>
2021-10-17 10:54:40 +0530

    added some core files

---
+++ lib/core/responsive_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'plaform.dart';
+
+abstract class View extends StatefulWidget {
+  const View({Key? key}) : super(key: key);
+}
+
+abstract class ResponsiveViewSate<Page extends View> extends State<Page> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return size.width < size.height
+        ? CurrentPlatForm.isIOS
+            ? iosView
+            : androidView
+        : webView;
+  }
+
+  Widget get webView;
+  Widget get iosView;
+  Widget get androidView;
+}
